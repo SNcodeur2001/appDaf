a14723a5836499e3eb40f611f3e182024c5cde30
nabzey <zeynabba45@gmail.com>
2025-07-22 23:36:06 +0000

    mise a jour

--- public/index.php
+++ public/index.php
@@ -4,10 +4,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use App\Core\Router;
 
 try {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
     $dotenv->load();
 } catch (Exception $e) {
-    // Gérer erreur .env
+    // Gérer l'erreur .env ici si nécessaire
 }
+
+// 💡 Charger les routes AVANT de résoudre la requête
+require_once __DIR__ . '/../app/routes/routes.php';
 
 Router::resolve();
