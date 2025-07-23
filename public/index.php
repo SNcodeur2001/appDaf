220485675292397f34867b8bfa30623dec258797
nabzey <zeynabba45@gmail.com>
2025-07-23 10:28:01 +0000

    mise a jour

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../../app/config/env.php';
+require_once __DIR__ . '/../app/config/env.php';
 use App\Core\Router;
 
 try {
